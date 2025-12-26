b5ab8bb5b07541e9c26d0a474baa21154f58ea3f
Sai-Santosh-Pal <saisantoshpal2010@gmail.com>
2025-12-26 21:46:35 +0530

    initial commit - i forgot to commit lol

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import Welcome from './components/pages/Welcome.vue'
+  import Layout from './components/layouts/Layout.vue'
+  import Dashboard from './components/pages/Dashboard.vue'
+  import Workout from './components/pages/Workout.vue'
+
+const selectDisplay = 2
+</script>
+
+<template>
+  <Layout>
+    <Welcome v-if="selectDisplay == 1" />
+    <Dashboard v-if="selectDisplay == 2" />
+    <Workout v-if="selectDisplay == 3" />
+  </Layout>
+</template>
+
+<style scoped>
+
+</style>
