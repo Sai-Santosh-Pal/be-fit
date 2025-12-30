4dec048fb5847e75b4051b72f6c3247d613dab36
Sai-Santosh-Pal <saisantoshpal2010@gmail.com>
2025-12-30 17:10:34 +0530

    exercise description ui and logic done

--- src/components/pages/Workout.vue
+++ src/components/pages/Workout.vue
@@ -1,11 +1,30 @@
 <script setup>
-    import { workoutProgram } from '../../utils';
+    import Portal from '../Portal.vue'
+    import { workoutProgram, exerciseDescriptions } from '../../utils';
+    import { computed, ref } from 'vue';
     const selectedWorkout = 4
     const { workout, warmup } = workoutProgram[selectedWorkout]
+    let selectedExercise = ref(null)
+    const exerciseDescription = computed(() => exerciseDescriptions[selectedExercise.value])
+
+
+    function handleCloseModal() {
+        selectedExercise.value = null
+    }
 
 </script>
 
 <template>
+    <Portal v-if="selectedExercise">
+        <div class="exercise-description">
+            <h3>{{ selectedExercise }}</h3>
+            <div>
+                <small>Description</small>
+                <p>{{ exerciseDescription }}</p>
+            </div>
+            <button @click="handleCloseModal">Close <i class="fa-solid fa-xmark"></i></button>
+        </div>
+    </Portal>
     <section id="workout-card">
         <div class="plan-card card">
             <div class="plan-card-header">
@@ -23,7 +42,9 @@
             <div class="workout-grid-row" v-for="(w, wIdx) in warmup" :key="wIdx">
                 <div class="grid-name">
                     <p>{{ w.name }}</p>
-                    <button>
+                    <button @click="() => {
+                        selectedExercise = w.name
+                    }">
                         <i class="fa-regular fa-circle-question"></i>
                     </button>
                 </div>
@@ -39,7 +60,9 @@
             <div class="workout-grid-row" v-for="(w, wIdx) in workout" :key="wIdx">
                 <div class="grid-name">
                     <p>{{ w.name }}</p>
-                    <button>
+                    <button @click="() => {
+                        selectedExercise = w.name
+                    }">
                         <i class="fa-regular fa-circle-question"></i>
                     </button>
                 </div>
@@ -129,5 +152,21 @@
 
     .workout-btns button i {
         padding-left: 0.5rem;
+    }
+
+    .exercise-description {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        width: 100%;
+    }
+
+    .exercise-description h3 {
+        text-transform: capitalize;
+    }
+
+    .exercise-description button i {
+        padding-left: 0.5rem;
+
     }
 </style>
